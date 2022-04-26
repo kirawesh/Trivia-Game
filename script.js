af03b7a2a89fcd000bf490260a407eea9cf834a2
kirawesh <kira.wiz@gmail.com>
2022-04-26 19:27:48 +0300

    26.4.22

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+let points = 0;
+
 function error(id,errortext) {
     document.getElementById(id).style.backgroundColor = "pink";
     document.getElementById("errortext").innerHTML = errortext;
+    points-=1;
+    document.getElementById("score").innerHTML = points;
 }
 
 function success(id) {
     document.getElementById(id).style.backgroundColor = "lightgreen";
     document.getElementById("errortext").innerHTML = "כל הכבוד";
+    points+=4;
+    document.getElementById("score").innerHTML = points;
 }
