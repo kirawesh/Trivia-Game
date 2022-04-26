08b2a4b672a953120bac9118ffa3e38ff2c279ac
kirawesh <kira.wiz@gmail.com>
2022-04-26 17:56:10 +0300

    26.4.22

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function error(id,errortext) {
-    document.getElementById(id).style.backgroundColor="pink";
+    document.getElementById(id).style.backgroundColor = "pink";
     document.getElementById("errortext").innerHTML = errortext;
 }
 
 function success(id) {
-    document.getElementById(id).style.backgroundColor="lightgreen";
-    document.getElementById("errortext").innerHTML = "";
-    document.getElementById(id).style.transform = "rotate(360deg)";
-    document.getElementById(id).style.transition = "all 3s";
-    
+    document.getElementById(id).style.backgroundColor = "lightgreen";
+    document.getElementById("errortext").innerHTML = "כל הכבוד";
 }
