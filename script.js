61e685d8aee06cc5cd5689ed7544e9c59ab26d33
kirawesh <kira.wiz@gmail.com>
2022-05-04 20:36:11 +0300

    Score 4.5.22

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let points = 0;
+let counter;
+
 
 function error(id,errortext) {
     document.getElementById(id).style.backgroundColor = "pink";
-    document.getElementById("errortext").innerHTML = errortext;
-    points-=1;
-    document.getElementById("score").innerHTML = points;
+    document.getElementById("errortext").innerHTML = errortext;  
+    counter = sessionStorage.getItem('counter');
+    sessionStorage.setItem('counter', counter-1);
+    document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
 }
 
 function success(id) {
     document.getElementById(id).style.backgroundColor = "lightgreen";
-    document.getElementById("errortext").innerHTML = "כל הכבוד";
-    points+=4;
-    document.getElementById("score").innerHTML = points;
+    document.getElementById("errortext").innerHTML = "כל הכבוד";        
+    counter = sessionStorage.getItem('counter');
+    sessionStorage.setItem('counter', counter*1+1);
+    document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
 }
