06f9b3da94c73b4da0b632d4fbcfc89ea85d78dc
kirawesh <kira.wiz@gmail.com>
2022-05-10 11:44:06 +0300

    10.5

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let counter;
 
 
-function error(id,errortext) {
-    document.getElementById(id).style.backgroundColor = "pink";
-    document.getElementById("errortext").innerHTML = errortext;  
+function error(id,font) {
     counter = sessionStorage.getItem('counter');
-    sessionStorage.setItem('counter', counter-1);
+    if(document.getElementById(id).style.backgroundColor == "pink"){
+    document.getElementById(id).style.backgroundColor = "";
+    sessionStorage.setItem('counter', counter+1);
     document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
 }
+    else {
+        document.getElementById(id).style.backgroundColor = "pink";
+        sessionStorage.setItem('counter', counter-1);
+        document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
+        sessionStorage.setItem('font1', font);
+    }
+    
+    // document.getElementById("errortext").innerHTML = errortext;  
+    
+    
+}
 
-function success(id) {
-    document.getElementById(id).style.backgroundColor = "lightgreen";
+function success(id,font) {
+    if(document.getElementById(id).style.backgroundColor == "lightgreen"){
+        document.getElementById(id).style.backgroundColor = "";}
+        else {document.getElementById(id).style.backgroundColor = "lightgreen";
+        sessionStorage.setItem('font1', font);}
     document.getElementById("errortext").innerHTML = "כל הכבוד";        
     counter = sessionStorage.getItem('counter');
     sessionStorage.setItem('counter', counter*1+1);
     document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
+    
 }
