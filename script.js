da80e4d6669d794179e3ed21c42053f3bbca23df
kirawesh <kira.wiz@gmail.com>
2022-05-18 08:17:35 +0300

    18.5.22

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-let counter;
-
-
-function error(id,font) {
-    counter = sessionStorage.getItem('counter');
-    if(document.getElementById(id).style.backgroundColor == "pink"){
-    document.getElementById(id).style.backgroundColor = "";
-    sessionStorage.setItem('counter', counter+1);
-    document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
-}
-    else {
-        document.getElementById(id).style.backgroundColor = "pink";
-        sessionStorage.setItem('counter', counter-1);
-        document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
-        sessionStorage.setItem('font1', font);
-    }
-    
-    // document.getElementById("errortext").innerHTML = errortext;  
-    
-    
+function click1() {
+    document.getElementById("opensanshebrew").style.display = 'block';
+    document.getElementById("briefMockup").style.display = 'none';
+    document.getElementById("rubik").style.display = 'none';
 }
 
-function success(id,font) {
-    if(document.getElementById(id).style.backgroundColor == "lightgreen"){
-        document.getElementById(id).style.backgroundColor = "";}
-        else {document.getElementById(id).style.backgroundColor = "lightgreen";
-        sessionStorage.setItem('font1', font);}
-    document.getElementById("errortext").innerHTML = "כל הכבוד";        
-    counter = sessionStorage.getItem('counter');
-    sessionStorage.setItem('counter', counter*1+1);
-    document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
-    
+function click2() {
+    document.getElementById("rubik").style.display = 'block';
+    document.getElementById("briefMockup").style.display = 'none';
+    document.getElementById("opensanshebrew").style.display = 'none';
 }
+
+
+
+
+// let counter;
+
+
+// function error(id,font) {
+//     counter = sessionStorage.getItem('counter');
+//     if(document.getElementById(id).style.backgroundColor == "pink"){
+//     document.getElementById(id).style.backgroundColor = "";
+//     sessionStorage.setItem('counter', counter+1);
+//     document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
+// }
+//     else {
+//         document.getElementById(id).style.backgroundColor = "pink";
+//         sessionStorage.setItem('counter', counter-1);
+//         document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
+//         sessionStorage.setItem('font1', font);
+//     }
+    
+//     // document.getElementById("errortext").innerHTML = errortext;  
+    
+    
+// }
+
+// function success(id,font) {
+//     if(document.getElementById(id).style.backgroundColor == "lightgreen"){
+//         document.getElementById(id).style.backgroundColor = "";}
+//         else {document.getElementById(id).style.backgroundColor = "lightgreen";
+//         sessionStorage.setItem('font1', font);}
+//     document.getElementById("errortext").innerHTML = "כל הכבוד";        
+//     counter = sessionStorage.getItem('counter');
+//     sessionStorage.setItem('counter', counter*1+1);
+//     document.getElementById("score").innerHTML = sessionStorage.getItem('counter');
+    
+// }
